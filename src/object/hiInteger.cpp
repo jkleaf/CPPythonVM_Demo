5c08452197082b351738b504fdbebd6db4714870
DestinyFlame <jk_leaf@163.com>
2021-10-05 20:36:48 +0800

    add some base class

---
+++ src/object/hiInteger.cpp
@@ -0,0 +1,8 @@
+#include "hiInteger.hpp"
+
+#include <stdio.h>
+
+HiInteger::HiInteger(int x)
+{
+    _value = x;
+}
---
+++ src/object/hiInteger.hpp
@@ -0,0 +1,16 @@
+#ifndef _HI_INTEGER_HPP
+#define _HI_INTEGER_HPP
+
+#include "object/hiObject.hpp"
+
+class HiInteger : public HiObject
+{
+private:
+    int _value;
+
+public:
+    HiInteger(int x);
+    int value() { return _value; }
+};
+
+#endif
